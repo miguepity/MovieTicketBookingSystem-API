c802aa89ef29ca2180a596052d454d40a9a7f3f0
fer-hnndz <jfernandohernandez28@gmail.com>
2026-05-27 17:40:12 -0600

    finished implementing /auth/login endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt/dist/jwt.service';
 
 @Injectable()
 export class AuthService {
-    login(email: string, password: string) {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
+  async login(email: string, password: string) {
+    const user = await this.usersService.findOneByEmail(email);
+    const hashed_password = await bcrypt.hash(password, 10);
+
+    if (!user || user.password_hash !== hashed_password) {
+      throw new BadRequestException('Invalid credentials');
     }
+
+    const payload = { userId: user.id, email: user.email, role: user.id_rol };
+
+    return { access_token: this.jwtService.signAsync(payload) };
+  }
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/scripts/default-index.js';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    console.debug(
+      '[PrismaService] Initializing PrismaClient with connection string:',
+      connectionString,
+    );
+    super({
+      adapter: new PrismaPg({ connectionString }),
+    });
+  }
+  
+  async onModuleInit() {
+    await this.$connect();
+    console.log('[PrismaService] Connected to the database!');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
