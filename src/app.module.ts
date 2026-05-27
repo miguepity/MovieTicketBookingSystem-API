f0bcbd757a81efed96f7bef2ffd2d709590c7900
William Steven Cole Paz <williamstevencole@gmail.com>
2026-05-27 17:36:24 -0600

    feat(prisma): added prisma migrations and prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { CiudadesModule } from './ciudades/ciudades.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, CiudadesModule],
   controllers: [AppController],
   providers: [AppService],
 })
