4ca14aeac958858f906089935bcd9b98ce97407e
Andrea Quin <andrea.quin123@gmail.com>
2026-05-27 18:34:29 -0600

    added endpoint for POST salas module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { CiudadesModule } from './ciudades/ciudades.module';
+import { SalasModule } from './modules/salas/salas.module';
 
 @Module({
-  imports: [PrismaModule, CiudadesModule],
+  imports: [PrismaModule, CiudadesModule, SalasModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/salas/salas.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { CreateSalaDto } from './dto/create-sala.dto';
+import { UpdateSalaDto } from './dto/update-sala.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+
+@Injectable()
+export class SalasService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createSalaDto: CreateSalaDto) {
+
+    const existing = await this.prisma.salas.findFirst({
+      where: { nombre: createSalaDto.nombre },
+      select: { id: true },
+    });
+
+    if (existing) {
+      throw new ConflictException(
+        `Ya existe una sala con el nombre "${createSalaDto.nombre}"`,
+      );
+    }
+    
+    const created = await this.prisma.salas.create({
+      data: { nombre: createSalaDto.nombre, id_cine: createSalaDto.id_cine, filas: createSalaDto.filas, columnas: createSalaDto.columnas },
+    });
+    return created;
+  }
+
+  findAll() {
+    return `This action returns all salas`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} sala`;
+  }
+
+  update(id: number, updateSalaDto: UpdateSalaDto) {
+    return `This action updates a #${id} sala`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} sala`;
+  }
+}
