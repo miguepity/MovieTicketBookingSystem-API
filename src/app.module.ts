bcdcec33bade82e668e7d92a44699bb0f627a1f0
William Steven Cole Paz <141452595+williamstevencole@users.noreply.github.com>
2026-05-27 18:28:22 -0600

    Import CiudadesModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { CineModule } from './cine/cine.module';
-//import { CiudadesModule } from './ciudades/ciudades.module';
+import { CiudadesModule } from './ciudades/ciudades.module';
 
 @Module({
-  imports: [PrismaModule, CineModule], //, CiudadesModule],
+  imports: [PrismaModule, CineModule, CiudadesModule],
   controllers: [AppController],
   providers: [AppService],
 })
