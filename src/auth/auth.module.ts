0adc96c4cbd0937dbf6c2c64c6b3c05abf37a655
DavidZelaya12 <davidzelaya06.isj@gmail.com>
2026-05-27 18:24:22 -0600

    Auth module inicialization

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'tu_secreto_aqui',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports: [JwtModule],
 })
-export class AuthModule {}
+export class AuthModule { }
