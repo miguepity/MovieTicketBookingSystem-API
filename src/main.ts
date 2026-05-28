b8c54634e20c954df801461767f1d8e6540d515d
AzaliaFlores19 <yamiflo19@unitec.edu>
2026-05-27 18:53:50 -0600

    Fixed swagger

--- src/main.ts
+++ src/main.ts
@@ -16,5 +16,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3000);
 }
+
+bootstrap();
