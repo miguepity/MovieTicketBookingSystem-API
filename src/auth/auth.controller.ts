eccb193c9fc36fcc95c07c4f8bea76b757c1f97f
Rpaz17 <rebeca.paz@unitec.edu>
2026-06-01 17:54:02 -0600

    Grupo 4 Adicion de Swagger en post register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { LoginDto } from './dto/login.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -11,6 +12,46 @@ export class AuthController {
     private authService: AuthService,
   ) {}
 
+  @ApiOperation({
+    description:
+      'Endpoint for user registration. Accepts email and password, creates a new user, and returns an access token.',
+    responses: {
+      201: {
+        description: 'User registered successfully',
+        content: {
+          'application/json': {
+            example: {
+              access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+            },
+          },
+        },
+      },
+      400: {
+        description: 'Bad Request - Email already in use or invalid input',
+        content: {
+          'application/json': {
+            example: {
+              statusCode: 400,
+              message: 'Email already in use',
+              error: 'Bad Request',
+            },
+          },
+        },
+      },
+      500: {
+        description: 'Internal Server Error',
+        content: {
+          'application/json': {
+            example: {
+              statusCode: 500,
+              message: 'An error occurred while registering the user',
+              error: 'Internal Server Error',
+            },
+          },
+        },
+      },
+    },
+  })
   @Post('register')
   async register(@Body() registerDto: RegisterDto) {
     const { email, password } = registerDto;
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,27 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    console.debug(
+      '[PrismaService] Initializing PrismaClient with connection string:',
+      connectionString,
+    );
+    super({
+      adapter: new PrismaPg({ connectionString }),
+    });
+  }
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
