afc7fe772f1f8e252dc52e45e7ce611c638d7c32
DavidZelaya12 <davidzelaya06.isj@gmail.com>
2026-06-01 18:35:22 -0600

    Add Brevo mail service and MailModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { CineModule } from './cine/cine.module';
 import { CiudadesModule } from './ciudades/ciudades.module';
 import { PeliculaModule } from './pelicula/pelicula.module';
 import { SalasModule } from './modules/salas/salas.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
     PrismaModule,
+    MailModule,
     CineModule,
     CiudadesModule,
     PeliculaModule,
