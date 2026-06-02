4b19fe9ec6fe797573b5f6b7e6aa73ceb40734c0
Gallowzz <134352607+Gallowzz@users.noreply.github.com>
2026-06-01 18:14:27 -0600

    Fixes to Peliculas

--- src/peliculas/peliculas.service.ts
+++ src/peliculas/peliculas.service.ts
@@ -1,10 +1,25 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePeliculaDto } from './dto/create-pelicula.dto';
 import { UpdatePeliculaDto } from './dto/update-pelicula.dto';
 
 @Injectable()
 export class PeliculasService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
+
+  async createPelicula(dto: CreatePeliculaDto) {
+    return await this.prisma.peliculas.create({
+      data: {
+        titulo: dto.titulo,
+        sinopsis: dto.sinopsis,
+        poster_url: dto.poster_url,
+        id_idioma: dto.idioma ? BigInt(dto.idioma) : null,
+        id_genero: dto.genero ? BigInt(dto.genero) : null,
+        fecha_estreno: dto.fecha_estreno,
+        id_usuario: BigInt(dto.uploaded_by),
+      },
+    });
+  }
 
   async getTitulo(titulo?: string) {
     if (!titulo) {
