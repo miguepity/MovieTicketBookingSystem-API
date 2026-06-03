9cf29debd7f97a72d5dff56f26b7e149089d1038
Liaf21 <liaf21ramirez@gmail.com>
2026-06-02 18:31:27 -0600

    Add Sala module, controller, service, and DTOs for managing salas

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { SalaModule } from './sala/sala.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     AuthModule,
+    SalaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
