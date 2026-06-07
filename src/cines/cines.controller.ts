e27ca18d536690fc3441863de58eaea5ee28f6ba
Jorge Paz <jorgefpaz011@gmail.com>
2026-06-07 00:28:48 -0600

    created endpoint get funciones disponibles por cine

--- src/cines/cines.controller.ts
+++ src/cines/cines.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Post, Param, Body, ParseIntPipe, Put } from "@nestjs/common";
+import { Controller, Get, Post, Param, Body, ParseIntPipe, Put } from "@nestjs/common";
 import { CineService } from "./cines.service";
 import { BodyDto } from "./dto/cine.body";
-import { ParamDto } from "./dto/cine.param";
 
 @Controller('cines')
 export class CineController{
@@ -16,6 +15,13 @@ export class CineController{
         }catch(error){
             return 'Internal server error';
         }
+    }
+
+    @Get(':id/funciones')
+    getFuncionesDisponibles(
+        @Param('id', ParseIntPipe) id: number
+    ){
+        return this.cineService.getFuncionesDisponibles(id);
     }
 
     @Put()
