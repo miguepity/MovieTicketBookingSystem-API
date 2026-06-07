18a24f4b19cd105607e176216916d2c4393fee5d
Liaf21 <liaf21ramirez@gmail.com>
2026-06-06 22:58:47 -0600

    fix: merging error changes not pulled from merge onto Grupo#2

--- src/admin/users/dto/query-usuarios.dto.ts
+++ src/admin/users/dto/query-usuarios.dto.ts
@@ -1,7 +1,14 @@
+import { IsOptional } from 'class-validator';
+
 export class QueryUsuariosDto {
+  @IsOptional()
   nombre?: string;
+  @IsOptional()
   email?: string;
+  @IsOptional()
   estado?: string;
+  @IsOptional()
   page?: string;
+  @IsOptional()
   limit?: string;
 }
