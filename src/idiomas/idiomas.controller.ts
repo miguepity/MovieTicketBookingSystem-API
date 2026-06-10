8a54a7c4a2a2e9d275b47018eeb0c39ca3b59c0e
nicky18-txt <nic.altamirano@unitec.edu>
2026-06-10 15:06:56 -0600

    post de idiomas

--- src/idiomas/idiomas.controller.ts
+++ src/idiomas/idiomas.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Post, UseGuards, Body, Controller } from '@nestjs/common';
+import { IdiomasService } from './idiomas.service';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { CreateIdiomaDto } from './dto/create-idioma.dto';
 
 @Controller('idiomas')
-export class IdiomasController {}
+export class IdiomasController {
+  constructor(private readonly idiomasService: IdiomasService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({
+    description: 'Crear un nuevo idioma',
+    responses: {
+      201: {
+        description: 'Idioma creado exitosamente',
+        content: {
+          'application/json': {
+            example: {
+              id: '1',
+              nombre: 'Español',
+              created_at: '2026-06-07T00:00:00.000Z',
+            },
+          },
+        },
+      },
+    },
+  })
+  create(@Body() dto: CreateIdiomaDto) {
+    return this.idiomasService.create(dto);
+  }
+}
