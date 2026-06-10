3255dc6ec856a1f7f6120bda78fa68c4350bef43
fer-hnndz <jfernandohernandez28@gmail.com>
2026-06-10 14:24:27 -0600

    fix modile import in pagos

--- src/pagos/pagos.module.ts
+++ src/pagos/pagos.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PagosService } from './pagos.service';
 import { PagosController } from './pagos.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   providers: [PagosService],
-  controllers: [PagosController]
+  controllers: [PagosController],
+  imports: [PrismaModule],
 })
 export class PagosModule {}
