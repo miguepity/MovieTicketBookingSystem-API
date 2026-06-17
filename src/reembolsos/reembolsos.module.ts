47434633134404d70060868f8e665ab695e6890d
Liaf21 <liaf21ramirez@gmail.com>
2026-06-16 18:35:10 -0600

    fix: quick fix

--- src/reembolsos/reembolsos.module.ts
+++ src/reembolsos/reembolsos.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { ReembolsosController } from './reembolsos.controller';
 import { ReembolsosService } from './reembolsos.service';
 import { MailModule } from '../mail/mail.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [PrismaModule, MailModule],
+    imports: [PrismaModule, MailModule, AuthModule],
     controllers: [ReembolsosController],
     providers: [ReembolsosService],
 })
